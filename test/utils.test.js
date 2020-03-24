108c26b8e13d9c4d3fab1c8ab7f714af243cc48a
Lucas Pereira <lucas_pereiracp@outlook.com.br>
2020-03-24 00:46:00 -0300

    bugs corrigidos e testes adicionados

--- test/utils.test.js
+++ test/utils.test.js
@@ -4,7 +4,30 @@ describe('Utils', () => {
     describe('gerarNumeroAleatorio', () => {
         test('fim nao pode ser negativo', () => {
             expect(gerarNumeroAleatorio(20, -5))
-                .toBe(1);
+                .toBe(-1);
+        });
+    });
+
+    describe('gerarNumeroAleatorio', () => {
+        test('inicio nao pode ser negativo', () => {
+            expect(gerarNumeroAleatorio(-5, 20))
+                .toBe(-1);
+        });
+    });
+
+    describe('gerarNumeroAleatorio', () => {
+        test('inicio nao pode ser maior que fim', () => {
+            expect(gerarNumeroAleatorio(20, 2))
+                .toBe(-1);
+        });
+    });
+
+
+
+    describe('gerarNumeroAleatorio', () => {
+        test('inicio e fim nao pode ser iguais', () => {
+            expect(gerarNumeroAleatorio(5, 5))
+                .toBe(-1);
         });
     });
 
@@ -13,5 +36,28 @@ describe('Utils', () => {
             expect(acharCaracter(-4, 'abcde', 'c'))
                 .toBe('comprimento invalido');
         });
-    });    
+    });
+
+    describe('acharCaracter', () => {
+        test('comprimento fornecido diferente do comprimento real', () => {
+            expect(acharCaracter(5, 'abc', 'c'))
+                .toBe('comprimento fornecido diferente do comprimento real');
+        });
+    });
+
+    describe('acharCaracter', () => {
+        test('retorna a posicao do caracter quando encontrado', () => {
+            expect(acharCaracter(3, 'abc', 'c'))
+                .toBe(2);
+        });
+    });
+
+    describe('acharCaracter', () => {
+        test('mensagem indicando que o caractera nao esta presente na cadeia', () => {
+            expect(acharCaracter(3, 'abc', 'd'))
+                .toBe('caracter nao esta presente');
+        });
+    });
+    
+    
 });
